b09424c5c3dc90469ddce7f59f19696866fa54a5
ssc19940105 <ssc19940105@users.noreply.github.com>
2024-12-19 22:23:30 +0800

    UI Refine

--- Witcher3StringEditor/Dialogs/Views/DeleteDataDialog.xaml.cs
+++ Witcher3StringEditor/Dialogs/Views/DeleteDataDialog.xaml.cs
@@ -1,28 +1,12 @@
-﻿using Syncfusion.UI.Xaml.Grid;
-
-namespace Witcher3StringEditor.Dialogs.Views;
+﻿namespace Witcher3StringEditor.Dialogs.Views;
 
 /// <summary>
 ///     DeleteDataDialog.xaml 的交互逻辑
 /// </summary>
 public partial class DeleteDataDialog
 {
-    private readonly GridRowSizingOptions gridRowResizingOptions
-        = new() { ExcludeColumns = ["StrID", "KeyHex", "KeyName"] };
-
-    //To get the calculated height from GetAutoRowHeight method.
-    private double autoHeight = double.NaN;
-
     public DeleteDataDialog()
     {
         InitializeComponent();
-    }
-
-    private void DataGrid_QueryRowHeight(object sender, QueryRowHeightEventArgs e)
-    {
-        if (!DataGrid.GridColumnSizer.GetAutoRowHeight(e.RowIndex, gridRowResizingOptions, out autoHeight)) return;
-        if (!(autoHeight > 24)) return;
-        e.Height = autoHeight;
-        e.Handled = true;
     }
 }
--- Witcher3StringEditor/Dialogs/Views/LogDialog.xaml.cs
+++ Witcher3StringEditor/Dialogs/Views/LogDialog.xaml.cs
@@ -1,26 +1,12 @@
-﻿using Syncfusion.UI.Xaml.Grid;
-
-namespace Witcher3StringEditor.Dialogs.Views;
+﻿namespace Witcher3StringEditor.Dialogs.Views;
 
 /// <summary>
 ///     LogDialog.xaml 的交互逻辑
 /// </summary>
 public partial class LogDialog
 {
-    //To get the calculated height from GetAutoRowHeight method.
-    private double autoHeight = double.NaN;
-
     public LogDialog()
     {
         InitializeComponent();
-    }
-
-    private void DataGrid_QueryRowHeight(object sender, QueryRowHeightEventArgs e)
-    {
-        if (!DataGrid.GridColumnSizer.GetAutoRowHeight(e.RowIndex, new GridRowSizingOptions(), out autoHeight))
-            return;
-        if (!(autoHeight > 30)) return;
-        e.Height = autoHeight;
-        e.Handled = true;
     }
 }
