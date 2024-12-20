f7fbf56e8cf8a49597e69907c69de53d25260f21
ssc19940105 <ssc19940105@users.noreply.github.com>
2024-12-21 02:53:15 +0800

    Co

---
+++ Witcher3StringEditor/Dialogs/Behaviors/SfDataGridAutoScrollToEndBehavior.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.ScrollAxis;
+using System.Windows;
+
+namespace Witcher3StringEditor.Dialogs.Behaviors;
+
+public class SfDataGridAutoScrollToEndBehavior : Behavior<SfDataGrid>
+{
+    protected override void OnAttached()
+    {
+        AssociatedObject.Loaded += AssociatedObject_Loaded;
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.Loaded -= AssociatedObject_Loaded;
+    }
+
+    private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+    {
+        var lastItem = AssociatedObject.View.Records.LastOrDefault();
+        if (lastItem == null) return;
+        var rowIndex = AssociatedObject.ResolveToRowIndex(lastItem);
+        AssociatedObject.ScrollInView(new RowColumnIndex(rowIndex, 0));
+    }
+}
