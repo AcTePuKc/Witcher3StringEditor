57165bb7ed67a1553e1cd4ee687fbb9295b34c5c
ssc19940105 <ssc19940105@users.noreply.github.com>
2024-12-21 05:30:29 +0800

    UI Refine

---
+++ Witcher3StringEditor/Dialogs/Behaviors/SfDataGridSizeChangedBehavior.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.Grid.Helpers;
+
+namespace Witcher3StringEditor.Dialogs.Behaviors;
+
+internal class SfDataGridSizeChangedBehavior : Behavior<SfDataGrid>
+{
+    protected override void OnAttached()
+    {
+        AssociatedObject.SizeChanged += AssociatedObject_SizeChanged;
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.SizeChanged -= AssociatedObject_SizeChanged;
+    }
+
+    private void AssociatedObject_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+    {
+        AssociatedObject.GetVisualContainer().RowHeightManager.Reset();
+        AssociatedObject.GetVisualContainer().InvalidateMeasureInfo();
+    }
+}
