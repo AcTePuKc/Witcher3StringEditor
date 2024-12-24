22f183b40f7321aec60eba9a24815ae27410b549
ssc19940105 <ssc19940105@users.noreply.github.com>
2024-12-25 00:44:40 +0800

    UI Refine

--- Witcher3StringEditor.Dialogs/Views/RecentDialog.xaml.cs
+++ Witcher3StringEditor.Dialogs/Views/RecentDialog.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Witcher3StringEditor.Dialogs.Views;
+﻿using iNKORE.UI.WPF.Modern.Controls;
+using System.Windows.Controls;
+
+namespace Witcher3StringEditor.Dialogs.Views;
 
 /// <summary>
 /// RecentDialog.xaml 的交互逻辑
@@ -8,5 +11,17 @@ public partial class RecentDialog
     public RecentDialog()
     {
         InitializeComponent();
+
+        SfDataGrid.SearchHelper.AllowFiltering = true;
+    }
+
+    private void SearchBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+    {
+        SfDataGrid.SearchHelper.Search(args.QueryText);
+    }
+
+    private void SearchBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+    {
+        if (string.IsNullOrEmpty(sender.Text)) SfDataGrid.SearchHelper.ClearSearch();
     }
 }
