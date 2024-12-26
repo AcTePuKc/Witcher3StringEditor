92eca3989d7039c93900de3fafdb61a0f3886bb5
ssc19940105 <ssc19940105@users.noreply.github.com>
2024-12-26 12:31:05 +0800

    Code Refine

--- Witcher3StringEditor.Dialogs/Models/TranslateItem.cs
+++ Witcher3StringEditor.Dialogs/Models/TranslateItem.cs
@@ -5,7 +5,7 @@ namespace Witcher3StringEditor.Dialogs.Models;
 public partial class TranslateItem : ObservableObject
 {
 
-    public required string Id { get; init; }
+    public required Guid Id { get; init; }
 
     public required string Text { get; set; }
 
