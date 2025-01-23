c864330a8d2c7c2c02b668e01775a483b8c1fe04
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-01-23 20:01:53 +0800

    Code Refine

--- Witcher3StringEditor.Dialogs/ViewModels/EditDataDialogViewModel.cs
+++ Witcher3StringEditor.Dialogs/ViewModels/EditDataDialogViewModel.cs
@@ -9,7 +9,7 @@ namespace Witcher3StringEditor.Dialogs.ViewModels;
 public partial class EditDataDialogViewModel(IW3Item w3Item)
     : ObservableObject, IModalDialogViewModel, ICloseable
 {
-    public string Title { get; } = w3Item.StrId == string.Empty ? Strings.AddDialogTitle : Strings.EditDialogTitle;
+    public string Title { get; } = string.IsNullOrWhiteSpace(w3Item.StrId) ? Strings.AddDialogTitle : Strings.EditDialogTitle;
 
     public IW3Item? W3Item { get; } = w3Item.Clone() as IW3Item;
 
