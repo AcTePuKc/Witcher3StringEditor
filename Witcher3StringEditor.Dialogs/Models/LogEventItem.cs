cc30f0b2fbdb688aabb3db6f619efb0356c498f0
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-01-26 17:20:02 +0800

    UI Refine

---
+++ Witcher3StringEditor.Dialogs/Models/LogEventItem.cs
@@ -0,0 +1,15 @@
+﻿using Serilog.Events;
+
+namespace Witcher3StringEditor.Dialogs.Models
+{
+    public class LogEventItem(LogEvent logEvent)
+    {
+        public LogEvent EventEntry => logEvent;
+
+        public DateTimeOffset Timestamp => logEvent.Timestamp;
+
+        public LogEventLevel Level => logEvent.Level;
+
+        public string Message => logEvent.RenderMessage();
+    }
+}
--- Witcher3StringEditor.Dialogs/ViewModels/LogDialogViewModel.cs
+++ Witcher3StringEditor.Dialogs/ViewModels/LogDialogViewModel.cs
@@ -1,13 +1,42 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using HanumanInstitute.MvvmDialogs;
 using Serilog.Events;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Witcher3StringEditor.Dialogs.Models;
 
 namespace Witcher3StringEditor.Dialogs.ViewModels;
 
-public class LogDialogViewModel(IEnumerable<LogEvent> logEvents) 
+public class LogDialogViewModel
     : ObservableObject, IModalDialogViewModel
 {
     public bool? DialogResult => true;
 
-    public IEnumerable<LogEvent> LogEvents { get; } = logEvents;
+    public ObservableCollection<LogEventItem> LogEvents { get; } = [];
+
+    public LogDialogViewModel(ObservableCollection<LogEvent> logEvents)
+    {
+        foreach (var logEvent in logEvents)
+            LogEvents.Add(new LogEventItem(logEvent));
+        logEvents.CollectionChanged += (s, e) =>
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems != null)
+            {
+                foreach (LogEvent item in e.NewItems)
+                {
+                    LogEvents.Add(new LogEventItem(item));
+                }
+            }
+        };
+        LogEvents.CollectionChanged += (s, e) =>
+        {
+            if (e.Action == NotifyCollectionChangedAction.Remove && e.OldItems != null)
+            {
+                foreach (LogEventItem item in e.OldItems)
+                {
+                    logEvents.Remove(item.EventEntry);
+                }
+            }
+        };
+    }
 }
