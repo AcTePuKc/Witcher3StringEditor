537eebeb96e616f3e797d9666758b403bd177d8e
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-01-26 15:23:35 +0800

    UI Refine

---
+++ Witcher3StringEditor.Dialogs/Converters/LogEventToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using Serilog.Events;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Witcher3StringEditor.Dialogs.Converters
+{
+    internal class LogEventToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is LogEvent logEvent)
+            {
+                // 调用 A 类中的方法
+                return logEvent.RenderMessage();
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
