96d8a8a70ce9113881fcc766a8de849685804e87
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-01-30 03:31:45 +0800

    refactor(Services): 优化 CheckUpdateService 中的代码结构
    - 将 Address 改为小写的 address，符合 C# 命名规范
    - 优化了代码格式，提高了可读性

--- Witcher3StringEditor/Services/CheckUpdateService.cs
+++ Witcher3StringEditor/Services/CheckUpdateService.cs
@@ -6,7 +6,7 @@ namespace Witcher3StringEditor.Services;
 
 internal class CheckUpdateService(IAppSettings appSettings) : ICheckUpdateService
 {
-    private readonly string Address = appSettings.NexusModUrl;
+    private readonly string address = appSettings.NexusModUrl;
     private const string Selectors = "#pagetitle>ul.stats.clearfix>li.stat-version>div>div.stat";
 
     public async Task<bool> CheckUpdate()
@@ -14,7 +14,7 @@ internal class CheckUpdateService(IAppSettings appSettings) : ICheckUpdateServic
         try
         {
             using var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
-            using var document = await context.OpenAsync(Address);
+            using var document = await context.OpenAsync(address);
             var element = document.QuerySelector(Selectors);
             return element != null
                 && new Version(element.InnerHtml) > new Version(ThisAssembly.AssemblyFileVersion);
