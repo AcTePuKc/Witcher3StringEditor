d825d269f873d126155dfe021ef4304e381ed437
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-01-30 03:34:31 +0800

    refactor(LogEventItem): 优化日志事件项类的代码结构
    - 更新命名空间声明，使用单行格式
    - 移除多余的空行，使代码更加紧凑
    - 保持类结构和属性定义的一致性

--- Witcher3StringEditor.Dialogs/Models/LogEventItem.cs
+++ Witcher3StringEditor.Dialogs/Models/LogEventItem.cs
@@ -1,15 +1,14 @@
 ﻿using Serilog.Events;
 
-namespace Witcher3StringEditor.Dialogs.Models
+namespace Witcher3StringEditor.Dialogs.Models;
+
+public class LogEventItem(LogEvent logEvent)
 {
-    public class LogEventItem(LogEvent logEvent)
-    {
-        public LogEvent EventEntry => logEvent;
+    public LogEvent EventEntry => logEvent;
 
-        public DateTimeOffset Timestamp => logEvent.Timestamp;
+    public DateTimeOffset Timestamp => logEvent.Timestamp;
 
-        public LogEventLevel Level => logEvent.Level;
+    public LogEventLevel Level => logEvent.Level;
 
-        public string Message => logEvent.RenderMessage();
-    }
+    public string Message => logEvent.RenderMessage();
 }
