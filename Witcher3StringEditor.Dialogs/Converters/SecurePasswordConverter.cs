6ab8f0d01e42d019f6773dbf2835347514064118
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-02-12 20:44:18 +0800

    feat(dialog): 添加 SecurePasswordConverter以加密 API 密码
    - 在 SettingsDialog.xaml 中添加 SecurePasswordConverter
    - 创建 SecurePasswordConverter 类实现 IValueConverter 接口- 使用 ProtectedData 类对密码进行加密和解密
    -确保在转换过程中处理异常情况

---
+++ Witcher3StringEditor.Dialogs/Converters/SecurePasswordConverter.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Witcher3StringEditor.Dialogs.Converters;
+
+public class SecurePasswordConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        try
+        {
+            if (value is not string encryptedPassword || string.IsNullOrWhiteSpace(encryptedPassword))
+                return DependencyProperty.UnsetValue;
+            var encryptedData = System.Convert.FromBase64String(encryptedPassword);
+            var data = ProtectedData.Unprotect(encryptedData, null, DataProtectionScope.CurrentUser);
+            var c= Encoding.UTF8.GetString(data);
+            return c;
+        }
+        catch (CryptographicException ex)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        try
+        {
+            if (value is not string password || string.IsNullOrWhiteSpace(password))
+                return DependencyProperty.UnsetValue;
+            var data = Encoding.UTF8.GetBytes(password);
+            var encryptedData = ProtectedData.Protect(data, null, DataProtectionScope.CurrentUser);
+            return System.Convert.ToBase64String(encryptedData);
+        }
+        catch (CryptographicException ex)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
