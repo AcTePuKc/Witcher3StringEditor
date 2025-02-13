d9fb9d4c0ad92c9a56c545b8208bd18dd7677325
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-02-13 11:58:17 +0800

    feat(Dialogs): 添加模型设置验证器
    - 实现了 ModelSettingsValidator 类，用于验证模型设置的有效性
    - 添加了对 EndPoint、ModelId、ApiKey 和 Prompts 字段的验证规则
    - 使用 FluentValidation 库进行验证

---
+++ Witcher3StringEditor.Dialogs/Validators/ModelSettingsValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System.IO;
+using Witcher3StringEditor.Interfaces;
+
+namespace Witcher3StringEditor.Dialogs.Validators;
+
+public class ModelSettingsValidator : AbstractValidator<IModelSettings>
+{
+    private static readonly Lazy<ModelSettingsValidator> LazyInstance
+    = new(static () => new ModelSettingsValidator());
+
+    public static ModelSettingsValidator Instance => LazyInstance.Value;
+
+    private ModelSettingsValidator()
+    {
+        RuleFor(x => x.EndPoint).Must(x => Uri.TryCreate(x, UriKind.Absolute, out var _));
+        RuleFor(x => x.ModelId).NotEmpty();
+        RuleFor(x=>x.ApiKey).NotEmpty();
+        RuleFor(x=>x.Prompts).NotEmpty();
+    }
+}
