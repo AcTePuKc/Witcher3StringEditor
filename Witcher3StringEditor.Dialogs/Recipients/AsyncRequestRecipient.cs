bf1727f014ba05c8a97fb53f66a4cf3c49c0370f
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-02-16 22:26:46 +0800

    feat(Dialogs): 添加 AsyncRequestRecipient 类
    添加了 AsyncRequestRecipient 类，用于处理异步请求消息。该类实现了 IRecipient<AsyncRequestMessage<T>> 接口，可以接收和处理 AsyncRequestMessage 类型的消息。

---
+++ Witcher3StringEditor.Dialogs/Recipients/AsyncRequestRecipient.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace Witcher3StringEditor.Dialogs.Recipients;
+
+public class AsyncRequestRecipient<T> : IRecipient<AsyncRequestMessage<T>>
+{
+    public void Receive(AsyncRequestMessage<T> message)
+    {
+    }
+}
