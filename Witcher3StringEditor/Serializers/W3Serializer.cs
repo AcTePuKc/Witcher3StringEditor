dda7c8a73a794ba4d159d199c732dc7b515adf28
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-02-24 20:22:54 +0800

    refactor(Witcher3StringEditor): 优化 Excel 文件读取代码
    - 简化了 Excel 文件打开和工作表加载的代码
    - 提高了代码的可读性和维护性

--- Witcher3StringEditor/Serializers/W3Serializer.cs
+++ Witcher3StringEditor/Serializers/W3Serializer.cs
@@ -72,9 +72,7 @@ internal class W3Serializer(IAppSettings appSettings, IBackupService backupServi
         try
         {
             using var excelEngine = new ExcelEngine();
-            var application = excelEngine.Excel;
-            IWorkbook workbook = application.Workbooks.Open(path);
-            var worksheet = workbook.Worksheets[0];
+            var worksheet = excelEngine.Excel.Workbooks.Open(path).Worksheets[0];
             var usedRange = worksheet.UsedRange;
             return worksheet.ExportData<W3Item>(1, 1, usedRange.LastRow, usedRange.LastColumn);
         }
