6629c66f2807edf812d4c6c6c9c639ca6f74dccc
ssc19940105 <ssc19940105@users.noreply.github.com>
2025-02-27 01:41:49 +0800

    feat: 添加日志记录功能- 在设置对话框中选择 encoder 路径和游戏路径时添加了日志记录 - 使用 Serilog 记录信息级别的日志 - 日志消息包括路径设置的类型和具体路径

--- Witcher3StringEditor.Dialogs/ViewModels/SettingDialogViewModel.cs
+++ Witcher3StringEditor.Dialogs/ViewModels/SettingDialogViewModel.cs
@@ -5,6 +5,7 @@ using CommunityToolkit.Mvvm.Messaging.Messages;
 using FluentValidation;
 using HanumanInstitute.MvvmDialogs;
 using HanumanInstitute.MvvmDialogs.FrameworkDialogs;
+using Serilog;
 using System.ComponentModel;
 using System.Windows;
 using Witcher3StringEditor.Dialogs.Locales;
@@ -32,7 +33,10 @@ public partial class SettingDialogViewModel(
         };
         using var storageFile = await dialogService.ShowOpenFileDialogAsync(this, dialogSettings);
         if (storageFile is { Name: "w3strings.exe" })
+        {
             AppSettings.W3StringsPath = storageFile.LocalPath;
+            Log.Information("Encoder Path set to '{0}'.", storageFile.LocalPath);
+        }
     }
 
     [RelayCommand]
@@ -46,7 +50,10 @@ public partial class SettingDialogViewModel(
         };
         using var storageFile = await dialogService.ShowOpenFileDialogAsync(this, dialogSettings);
         if (storageFile is { Name: "witcher3.exe" })
+        {
             AppSettings.GameExePath = storageFile.LocalPath;
+            Log.Information("Game Path set to '{0}'.", storageFile.LocalPath);
+        }
     }
 
     [RelayCommand]
