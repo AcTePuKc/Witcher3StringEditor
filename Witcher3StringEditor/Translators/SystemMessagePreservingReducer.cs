a2cd9b00dfb7f2ac6b94ecbbb0881dfd5e7e1046
ssc19940105 <ssc19940105@163.com>
2025-08-14 22:31:27 +0800

    refactor(Witcher3StringEditor): 优化 SystemMessagePreservingReducer 类的实现
    - 移除了不必要的命名空间括号
    - 使用了单行命名空间声明
    - 删除了多余的空行
    -简化了代码结构，提高了代码的可读性和维护性

--- Witcher3StringEditor/Translators/SystemMessagePreservingReducer.cs
+++ Witcher3StringEditor/Translators/SystemMessagePreservingReducer.cs
@@ -1,45 +1,44 @@
 ﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 
-namespace Witcher3StringEditor.Translators
+namespace Witcher3StringEditor.Translators;
+
+internal class SystemMessagePreservingReducer : IChatHistoryReducer
 {
-    internal class SystemMessagePreservingReducer : IChatHistoryReducer
-    {
-        private readonly int maxNonSystemMessages;
+    private readonly int maxNonSystemMessages;
         
-        public SystemMessagePreservingReducer(int maxNonSystemMessages)
+    public SystemMessagePreservingReducer(int maxNonSystemMessages)
+    {
+        if (maxNonSystemMessages < 0)
+            throw new ArgumentException(@"The maximum number of preserved messages cannot be negative", nameof(maxNonSystemMessages));
+        this.maxNonSystemMessages = maxNonSystemMessages;
+    }
+
+    Task<IEnumerable<ChatMessageContent>?> IChatHistoryReducer.ReduceAsync(IReadOnlyList<ChatMessageContent> chatHistory, CancellationToken cancellationToken)
+    {
+        var systemMessages = chatHistory
+            .Where(m => m.Role == AuthorRole.System)
+            .ToList();
+
+        var nonSystemMessages = chatHistory
+            .Where(m => m.Role != AuthorRole.System)
+            .ToList();
+
+        List<ChatMessageContent> truncatedNonSystemMessages;
+        if (nonSystemMessages.Count <= maxNonSystemMessages)
         {
-            if (maxNonSystemMessages < 0)
-                throw new ArgumentException(@"The maximum number of preserved messages cannot be negative", nameof(maxNonSystemMessages));
-            this.maxNonSystemMessages = maxNonSystemMessages;
+            truncatedNonSystemMessages = nonSystemMessages;
+        }
+        else
+        {
+            var skipCount = nonSystemMessages.Count - maxNonSystemMessages;
+            truncatedNonSystemMessages = [.. nonSystemMessages.Skip(skipCount)];
         }
 
-        Task<IEnumerable<ChatMessageContent>?> IChatHistoryReducer.ReduceAsync(IReadOnlyList<ChatMessageContent> chatHistory, CancellationToken cancellationToken)
-        {
-            var systemMessages = chatHistory
-                .Where(m => m.Role == AuthorRole.System)
-                .ToList();
+        var reducedHistory = new List<ChatMessageContent>();
+        reducedHistory.AddRange(systemMessages);
+        reducedHistory.AddRange(truncatedNonSystemMessages);
 
-            var nonSystemMessages = chatHistory
-                .Where(m => m.Role != AuthorRole.System)
-                .ToList();
-
-            List<ChatMessageContent> truncatedNonSystemMessages;
-            if (nonSystemMessages.Count <= maxNonSystemMessages)
-            {
-                truncatedNonSystemMessages = nonSystemMessages;
-            }
-            else
-            {
-                var skipCount = nonSystemMessages.Count - maxNonSystemMessages;
-                truncatedNonSystemMessages = [.. nonSystemMessages.Skip(skipCount)];
-            }
-
-            var reducedHistory = new List<ChatMessageContent>();
-            reducedHistory.AddRange(systemMessages);
-            reducedHistory.AddRange(truncatedNonSystemMessages);
-
-            return Task.FromResult<IEnumerable<ChatMessageContent>?>(reducedHistory);
-        }
+        return Task.FromResult<IEnumerable<ChatMessageContent>?>(reducedHistory);
     }
 }
