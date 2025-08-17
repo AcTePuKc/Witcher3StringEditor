ad96c83ced96723a175098091a91177b218cbc8d
ssc19940105 <ssc19940105@163.com>
2025-08-17 08:34:10 +0800

    feat(Dialogs): 实现知识对话框的视图模型功能
    - 添加 KnowledgeDialogViewModel 类的实现- 增加知识项目列表、搜索、学习、删除和清除功能
    - 使用 CommunityToolkit.Mvvm 和 iNKORE.UI.WPF.Modern 控件

--- Witcher3StringEditor.Dialogs/ViewModels/KnowledgeDialogViewModel.cs
+++ Witcher3StringEditor.Dialogs/ViewModels/KnowledgeDialogViewModel.cs
@@ -1,11 +1,47 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using HanumanInstitute.MvvmDialogs;
+using iNKORE.UI.WPF.Modern.Controls;
 using Witcher3StringEditor.Interfaces;
 
-namespace Witcher3StringEditor.Dialogs.ViewModels
+namespace Witcher3StringEditor.Dialogs.ViewModels;
+
+public partial class KnowledgeDialogViewModel(IKnowledgeService knowledgeService)
+    : ObservableObject, IModalDialogViewModel
 {
-    public class KnowledgeDialogViewModel(IKnowledgeService knowledgeService) : ObservableObject, IModalDialogViewModel
+    public bool? DialogResult => true;
+    
+    public ObservableCollection<IW3KItem> KnowledgeItems { get; } = [];
+
+    [RelayCommand]
+    private async Task Search(AutoSuggestBoxQuerySubmittedEventArgs eventArgs)
     {
-        public bool? DialogResult => true;
+        var text = eventArgs.QueryText;
+        if (string.IsNullOrWhiteSpace(text)) return;
+        var results = knowledgeService.Search(text, 10);
+        if (results == null) return;
+        if (KnowledgeItems.Any()) KnowledgeItems.Clear();
+        await foreach (var item in results) KnowledgeItems.Add(item);
+    }
+
+    [RelayCommand]
+    private async Task LearnAsync(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text)) return;
+        await knowledgeService.Learn(text);
+    }
+
+    [RelayCommand]
+    private async Task DeleteAsync(object[]? items)
+    {
+        if (items == null || items.Length == 0) return;
+        await knowledgeService.Delete(items.Cast<IW3KItem>().Select(x => x.Id));
+    }
+
+    [RelayCommand]
+    private async Task ClearAsync()
+    {
+        await knowledgeService.Clear();
     }
 }
