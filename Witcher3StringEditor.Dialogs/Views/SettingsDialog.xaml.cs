424a88a6ac233162816018fcf66c2f8ed89414a9
ssc19940105 <ssc19940105@163.com>
2025-08-18 17:04:18 +0800

    refactor(dialogs): 移除设置对话框的冗余代码- 删除了 SettingsDialog.xaml 中的 Closed 事件处理程序 -移除了 SettingsDialog.xaml.cs 中的多余 using 语句 - 删除了初始化中注册消息处理程序的代码 - 移除了 Window_Closed 事件处理方法

--- Witcher3StringEditor.Dialogs/Views/SettingsDialog.xaml.cs
+++ Witcher3StringEditor.Dialogs/Views/SettingsDialog.xaml.cs
@@ -1,43 +1,12 @@
-﻿using System.Windows;
-using CommunityToolkit.Mvvm.Messaging;
-using CommunityToolkit.Mvvm.Messaging.Messages;
-using Witcher3StringEditor.Dialogs.Locales;
-using Witcher3StringEditor.Dialogs.Recipients;
-using MessageBox = iNKORE.UI.WPF.Modern.Controls.MessageBox;
-
-namespace Witcher3StringEditor.Dialogs.Views;
+﻿namespace Witcher3StringEditor.Dialogs.Views;
 
 /// <summary>
 ///     SettingsDialog.xaml 的交互逻辑
 /// </summary>
 public partial class SettingsDialog
 {
-    private readonly AsyncRequestRecipient<bool> closingRecipient = new();
-
     public SettingsDialog()
     {
         InitializeComponent();
-
-        var messageHandlers = new[]
-        {
-            ("InitializationIncomplete", Strings.InitializationIncompleteMessage,
-                Strings.InitializationIncompleteCaption)
-        };
-
-        foreach (var (token, message, caption) in messageHandlers)
-            WeakReferenceMessenger.Default.Register<AsyncRequestRecipient<bool>, AsyncRequestMessage<bool>, string>(
-                closingRecipient,
-                token,
-                (r, m) =>
-                {
-                    r.Receive(m);
-                    m.Reply(MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Question) ==
-                            MessageBoxResult.Yes);
-                });
-    }
-
-    private void Window_Closed(object sender, EventArgs e)
-    {
-        WeakReferenceMessenger.Default.UnregisterAll(closingRecipient);
     }
 }
