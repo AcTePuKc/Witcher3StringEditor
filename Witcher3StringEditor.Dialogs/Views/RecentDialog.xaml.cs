14405b45745d0e74ea9a3e85f6136dff95f74b92
ssc19940105 <ssc19940105@163.com>
2025-08-24 18:33:46 +0800

    refactor(dialogs): 重构 RecentDialog 以使用 Microsoft.Extensions.Logging- 在 RecentDialog 中注入 Microsoft.Extensions.Logging.ILogger - 用 logger 替代 Serilog.Log 记录日志信息 - 通过 Ioc.Default 获取 logger 实例

--- Witcher3StringEditor.Dialogs/Views/RecentDialog.xaml.cs
+++ Witcher3StringEditor.Dialogs/Views/RecentDialog.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System.Windows;
+using CommunityToolkit.Mvvm.DependencyInjection;
 using CommunityToolkit.Mvvm.Messaging;
 using CommunityToolkit.Mvvm.Messaging.Messages;
 using iNKORE.UI.WPF.Modern.Controls;
-using Serilog;
+using Microsoft.Extensions.Logging;
 using Witcher3StringEditor.Dialogs.Locales;
 using Witcher3StringEditor.Dialogs.Recipients;
 using MessageBox = iNKORE.UI.WPF.Modern.Controls.MessageBox;
@@ -15,10 +16,12 @@ namespace Witcher3StringEditor.Dialogs.Views;
 public partial class RecentDialog
 {
     private readonly AsyncRequestRecipient<bool> recipient = new();
+    private readonly ILogger<RecentDialog> logger;
 
     public RecentDialog()
     {
         InitializeComponent();
+        logger = Ioc.Default.GetRequiredService<ILogger<RecentDialog>>();
         SfDataGrid.SearchHelper.AllowFiltering = true;
         WeakReferenceMessenger.Default.Register<AsyncRequestRecipient<bool>, AsyncRequestMessage<bool>, string>(
             recipient, "RecentItem", (r, m) =>
@@ -34,7 +37,7 @@ public partial class RecentDialog
     private void SearchBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
     {
         SfDataGrid.SearchHelper.Search(args.QueryText);
-        Log.Information("Search query submitted: {QueryText}", args.QueryText);
+        logger.LogInformation("Search query submitted: {QueryText}", args.QueryText);
     }
 
     private void SearchBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
