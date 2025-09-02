fb3414433d82a355dc9db8f4bbf8baa37a7f0996
ssc19940105 <ssc19940105@163.com>
2025-09-02 23:44:45 +0800

    refactor(Witcher3StringEditor): 优化 TranslatorToStringConverter 的代码结构
    - 简化了 Convert 方法的逻辑，提高了代码可读性
    - 在 Convert 和 ConvertBack 方法中添加了参数类型注解，增强了类型安全

--- Witcher3StringEditor.Dialogs/Converters/TranslatorToStringConverter.cs
+++ Witcher3StringEditor.Dialogs/Converters/TranslatorToStringConverter.cs
@@ -9,15 +9,14 @@ namespace Witcher3StringEditor.Dialogs.Converters;
 
 internal class TranslatorToStringConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is not string name) return DependencyProperty.UnsetValue;
-
-        var translator = Ioc.Default.GetServices<ITranslator>().First(x => x.Name == name);
-        return translator ?? DependencyProperty.UnsetValue;
+        return value is not string name
+            ? DependencyProperty.UnsetValue
+            : Ioc.Default.GetServices<ITranslator>().First(x => x.Name == name);
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is not ITranslator || targetType != typeof(string)) return DependencyProperty.UnsetValue;
 
