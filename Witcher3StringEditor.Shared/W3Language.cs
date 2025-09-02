ceb939d5cea8ddc077539a92ad9129d815a423f1
ssc19940105 <ssc19940105@163.com>
2025-09-02 23:53:25 +0800

    refactor(W3Language): 为枚举成员添加 UsedImplicitly属性
    - 在 W3Language枚举中，为多个成员添加了 [UsedImplicitly] 属性
    - 这个修改可以提高代码的可读性和维护性，同时避免潜在的未使用变量警告

--- Witcher3StringEditor.Shared/W3Language.cs
+++ Witcher3StringEditor.Shared/W3Language.cs
@@ -1,4 +1,6 @@
-﻿namespace Witcher3StringEditor.Shared;
+﻿using JetBrains.Annotations;
+
+namespace Witcher3StringEditor.Shared;
 
 public enum W3Language
 {
@@ -6,17 +8,17 @@ public enum W3Language
     br = 1,
     cn = 2,
     cz = 3,
-    de = 4,
-    en = 5,
-    es = 6,
+    [UsedImplicitly] de = 4,
+    [UsedImplicitly] en = 5,
+    [UsedImplicitly] es = 6,
     esmx = 7,
-    fr = 8,
-    hu = 9,
-    it = 10,
+    [UsedImplicitly] fr = 8,
+    [UsedImplicitly] hu = 9,
+    [UsedImplicitly] it = 10,
     jp = 11,
     kr = 12,
-    pl = 13,
-    ru = 14,
+    [UsedImplicitly] pl = 13,
+    [UsedImplicitly] ru = 14,
     zh = 15,
     tr = 16
 }
