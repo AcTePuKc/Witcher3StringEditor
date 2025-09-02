3e97c01209822af96829badbbea376c100806f54
ssc19940105 <ssc19940105@163.com>
2025-09-02 23:43:17 +0800

    refactor: 添加 UsedImplicitly属性到 IRecentItem 接口
    - 在 IRecentItem 接口中，为 OpenedTime 属性添加了 UsedImplicitly 属性- 这个改动可能有助于减少代码警告或提高代码的可维护性

--- Witcher3StringEditor.Shared/Abstractions/IRecentItem.cs
+++ Witcher3StringEditor.Shared/Abstractions/IRecentItem.cs
@@ -1,10 +1,12 @@
-﻿namespace Witcher3StringEditor.Shared.Abstractions;
+﻿using JetBrains.Annotations;
+
+namespace Witcher3StringEditor.Shared.Abstractions;
 
 public interface IRecentItem
 {
     public string FilePath { get; }
 
-    public DateTime OpenedTime { get; set; }
+    [UsedImplicitly] public DateTime OpenedTime { get; set; }
 
     public bool IsMarked { get; set; }
 }
