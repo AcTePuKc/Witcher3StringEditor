e37a7642800b6ea20d95291f12f35af595ae12fb
ssc19940105 <ssc19940105@163.com>
2025-09-03 00:04:22 +0800

    refactor(Witcher3StringEditor): 优化 W3JobModel属性初始化
    - 将 Path属性的 set 访问器改为 init 访问器
    -将 W3Items 属性的 set 访问器改为 init 访问器

--- Witcher3StringEditor.Dialogs/Models/W3JobModel.cs
+++ Witcher3StringEditor.Dialogs/Models/W3JobModel.cs
@@ -14,7 +14,7 @@ internal partial class W3JobModel : ObservableObject, IW3Job
     [ObservableProperty] private W3Language language;
     [ObservableProperty] private W3FileType w3FileType;
 
-    public required string Path { get; set; }
+    public required string Path { get; init; }
 
-    public required IEnumerable<IW3Item> W3Items { get; set; }
+    public required IEnumerable<IW3Item> W3Items { get; init; }
 }
