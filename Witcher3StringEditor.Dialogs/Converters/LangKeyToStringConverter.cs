c51df01f411ae05a51ac0a58c5d54903b4160808
ssc19940105 <ssc19940105@163.com>
2025-09-03 11:04:07 +0800

    refactor(dialogs): 优化翻译文本显示逻辑
    - 在 TranslateDataTemplate.xaml 中添加 LangKeyToStringConverter 转换器
    - 使用转换器处理翻译文本的显示，提高代码复用性和可维护性
    - 新增 LangKeyToStringConverter 类，实现从语言键到翻译字符串的转换

---
+++ Witcher3StringEditor.Dialogs/Converters/LangKeyToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Witcher3StringEditor.Dialogs.Converters;
+
+public class LangKeyToStringConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        try
+        {
+            return value is not string key ? DependencyProperty.UnsetValue : I18NExtension.Translate(key);
+        }
+        catch (Exception)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
