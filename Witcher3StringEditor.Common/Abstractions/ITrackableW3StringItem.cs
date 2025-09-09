938cafe0f62af4efa5e28c885f228baa7d7a2dd0
ssc19940105 <ssc19940105@163.com>
2025-09-09 23:15:11 +0800

    refactor(Common): 重命名接口 ITrackableW3Item 为 ITrackableW3StringItem- 将 ITrackableW3Item 接口重命名为 ITrackableW3StringItem -此更改是为了更清晰地表示该接口是用于可跟踪的 W3 字符串项

--- Witcher3StringEditor.Common/Abstractions/ITrackableW3StringItem.cs
+++ Witcher3StringEditor.Common/Abstractions/ITrackableW3StringItem.cs
@@ -1,6 +1,6 @@
 ﻿namespace Witcher3StringEditor.Common.Abstractions;
 
-public interface IEditableW3StringItem : IW3StringItem, ICloneable
+public interface ITrackableW3StringItem : IW3StringItem, ICloneable
 {
     public Guid Id { get; }
 }
