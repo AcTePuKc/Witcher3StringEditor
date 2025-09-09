965367bd63a2621207fca06b30048f08a3bbebf9
ssc19940105 <ssc19940105@163.com>
2025-09-09 13:56:34 +0800

    refactor(Witcher3StringEditor): 优化 AboutDialogViewModel 类的代码格式
    - 调整类定义的换行方式，提高代码可读性
    - 保持功能和逻辑不变，仅优化代码结构

--- Witcher3StringEditor.Dialogs/ViewModels/AboutDialogViewModel.cs
+++ Witcher3StringEditor.Dialogs/ViewModels/AboutDialogViewModel.cs
@@ -3,7 +3,8 @@ using HanumanInstitute.MvvmDialogs;
 
 namespace Witcher3StringEditor.Dialogs.ViewModels;
 
-public class AboutDialogViewModel(IReadOnlyDictionary<string, object?> aboutInfo) : ObservableObject, IModalDialogViewModel
+public class AboutDialogViewModel(IReadOnlyDictionary<string, object?> aboutInfo)
+    : ObservableObject, IModalDialogViewModel
 {
     public IReadOnlyDictionary<string, object?> AboutInfo => aboutInfo;
     public bool? DialogResult => true;
