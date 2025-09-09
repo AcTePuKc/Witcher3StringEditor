228c5128c19bdd167cce9bf08075fe04a62dea2a
ssc19940105 <ssc19940105@163.com>
2025-09-09 12:54:33 +0800

    refactor(Witcher3StringEditor): 优化关于对话框视图模型
    - 将 AboutDialogViewModel 中的 IDictionary 替换为 IReadOnlyDictionary
    - 此修改提高了代码的安全性和可维护性，防止意外修改字典内容

--- Witcher3StringEditor.Dialogs/ViewModels/AboutDialogViewModel.cs
+++ Witcher3StringEditor.Dialogs/ViewModels/AboutDialogViewModel.cs
@@ -3,8 +3,8 @@ using HanumanInstitute.MvvmDialogs;
 
 namespace Witcher3StringEditor.Dialogs.ViewModels;
 
-public class AboutDialogViewModel(IDictionary<string, object?> aboutInfo) : ObservableObject, IModalDialogViewModel
+public class AboutDialogViewModel(IReadOnlyDictionary<string, object?> aboutInfo) : ObservableObject, IModalDialogViewModel
 {
-    public IDictionary<string, object?> AboutInfo => aboutInfo;
+    public IReadOnlyDictionary<string, object?> AboutInfo => aboutInfo;
     public bool? DialogResult => true;
 }
