84c451ab54a2953d79a239ed6e935d9ba3a90592
ssc19940105 <ssc19940105@163.com>
2025-09-11 11:35:32 +0800

    feat(Dialogs): 为语言选择下拉框添加本地化描述- 新增 EnumToDescriptionConverter转换器，用于将枚举值转换为本地化描述- 在 SaveDialog 和 SettingsDialog 中使用新转换器，为语言选择下拉框添加本地化描述- 更新资源字典，添加 EnumToDescriptionConverter 实例

---
+++ Witcher3StringEditor.Dialogs/Converters/EnumToDescriptionConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Data;
+using Witcher3StringEditor.Common;
+
+namespace Witcher3StringEditor.Dialogs.Converters;
+
+public class EnumToDescriptionConverter:IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+      if(value is not W3Language language) return DependencyProperty.UnsetValue;
+      return new CultureInfo(typeof(W3Language).GetField(language.ToString())!
+          .GetCustomAttribute<DescriptionAttribute>()!.Description).NativeName;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
