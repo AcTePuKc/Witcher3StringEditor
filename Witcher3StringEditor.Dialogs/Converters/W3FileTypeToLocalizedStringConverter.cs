2fdf83d20e1aaec11b348e9cea8b553b657dfd5d
ssc19940105 <ssc19940105@163.com>
2025-09-11 20:26:35 +0800

    feat(dialogs): 为文件类型添加本地化显示文本
    - 在 SaveDialog 和 SettingsDialog 中，为文件类型下拉框添加本地化显示文本
    - 新增 W3FileTypeToLocalizedStringConverter 转换器，用于将文件类型枚举转换为本地化字符串
    - 更新对话框资源字典，添加新的转换器

---
+++ Witcher3StringEditor.Dialogs/Converters/W3FileTypeToLocalizedStringConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Witcher3StringEditor.Common;
+using Witcher3StringEditor.Locales;
+
+namespace Witcher3StringEditor.Dialogs.Converters;
+
+internal class W3FileTypeToLocalizedStringConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values[0] is not W3FileType fileType) return DependencyProperty.UnsetValue;
+        return fileType switch
+        {
+            W3FileType.Csv => Strings.FileFormatTextFile,
+            W3FileType.W3Strings => Strings.FileFormatWitcher3StringsFile,
+            W3FileType.Excel => Strings.FileFormatExcelWorkbook,
+            _ => DependencyProperty.UnsetValue
+        };
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
