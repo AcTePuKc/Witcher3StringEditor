def13b1c45456406cdb6cf4ca9e52b13500ae0c3
ssc19940105 <ssc19940105@163.com>
2025-09-11 20:26:47 +0800

    refactor(W3Language): 为部分语言代码添加 UsedImplicitly属性
    - 在 Cz、Jp、Zh 语言代码前添加 [UsedImplicitly] 属性- 此修改可能是为了提高代码分析工具的准确性，避免未使用的枚举值引发警告

--- Witcher3StringEditor.Common/W3Language.cs
+++ Witcher3StringEditor.Common/W3Language.cs
@@ -8,6 +8,8 @@ public enum W3Language
     [Description("ar")] Ar = 0,
     [Description("pt")] Br = 1,
     [Description("zh-Hans")] Cn = 2,
+
+    [UsedImplicitly]
     [Description("cs")] Cz = 3,
     [Description("de")] [UsedImplicitly] De = 4,
     [Description("en")] [UsedImplicitly] En = 5,
@@ -16,10 +18,14 @@ public enum W3Language
     [Description("fr")] [UsedImplicitly] Fr = 8,
     [Description("hu")] [UsedImplicitly] Hu = 9,
     [Description("it")] [UsedImplicitly] It = 10,
+
+    [UsedImplicitly]
     [Description("jp")] Jp = 11,
     [Description("ko")] Kr = 12,
     [Description("pl")] [UsedImplicitly] Pl = 13,
     [Description("ru")] [UsedImplicitly] Ru = 14,
+
+    [UsedImplicitly]
     [Description("zh-Hant")] Zh = 15,
     [Description("tr")] Tr = 16
 }
