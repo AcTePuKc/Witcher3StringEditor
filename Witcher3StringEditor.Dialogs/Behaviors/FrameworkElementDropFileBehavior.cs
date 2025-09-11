cdf394524114d2d2decd08244c1a86002c8671b2
ssc19940105 <ssc19940105@163.com>
2025-09-11 20:36:46 +0800

    refactor(Witcher3StringEditor): 移除不必要的注释- 删除了 FrameworkElementDropFileBehavior 类中 Data 属性的注释 - 该注释解释了 DataProperty 的用途，但并未提供额外的有价值信息

--- Witcher3StringEditor.Dialogs/Behaviors/FrameworkElementDropFileBehavior.cs
+++ Witcher3StringEditor.Dialogs/Behaviors/FrameworkElementDropFileBehavior.cs
@@ -6,7 +6,6 @@ namespace Witcher3StringEditor.Dialogs.Behaviors;
 
 public class FrameworkElementDropFileBehavior : Behavior<FrameworkElement>
 {
-    // Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
     public static readonly DependencyProperty DataProperty =
         DependencyProperty.Register(nameof(Data),
             typeof(string[]),
