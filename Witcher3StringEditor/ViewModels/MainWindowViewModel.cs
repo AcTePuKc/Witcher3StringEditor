7195c8d9427ca9b0a7897d78c81e78d09a2e5844
ssc19940105 <ssc19940105@163.com>
2025-09-17 01:40:12 +0800

    refactor: 移除未使用的 using 语句
    - 删除了 MainWindowViewModel.cs 文件中未使用的 using 语句
    - 优化了代码结构，提高了代码的可读性和维护性

--- Witcher3StringEditor/ViewModels/MainWindowViewModel.cs
+++ Witcher3StringEditor/ViewModels/MainWindowViewModel.cs
@@ -24,7 +24,6 @@ using Serilog.Events;
 using Syncfusion.Data.Extensions;
 using Witcher3StringEditor.Common.Abstractions;
 using Witcher3StringEditor.Dialogs.Messaging;
-using Witcher3StringEditor.Dialogs.Models;
 using Witcher3StringEditor.Dialogs.ViewModels;
 using Witcher3StringEditor.Locales;
 using Witcher3StringEditor.Models;
