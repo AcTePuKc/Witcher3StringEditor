4fb25f8c482dca364038d1bb773b09ec5f3fc7e0
ssc19940105 <ssc19940105@163.com>
2025-09-25 00:32:27 +0800

    refactor(logging): 将 LogEventItemModel 类重构为记录类型
    - 将类声明从 class 更改为 record
    - 更新构造函数参数名称的大小写- 修改属性访问以使用新的记录参数名称
    -保持所有公共 API 和功能不变

--- Witcher3StringEditor.Dialogs/Models/LogEventItemModel.cs
+++ Witcher3StringEditor.Dialogs/Models/LogEventItemModel.cs
@@ -8,29 +8,29 @@ namespace Witcher3StringEditor.Dialogs.Models;
 ///     A model class that wraps a Serilog LogEvent for display in the UI
 ///     Provides easy access to log event properties such as timestamp, level, and message
 /// </summary>
-/// <param name="logEvent">The Serilog LogEvent to wrap</param>
-public class LogEventItemModel(LogEvent logEvent)
+/// <param name="LogEvent">The Serilog LogEvent to wrap</param>
+public record LogEventItemModel(LogEvent LogEvent)
 {
     /// <summary>
     ///     Gets the original Serilog LogEvent
     /// </summary>
-    public LogEvent EventEntry => logEvent;
+    public LogEvent EventEntry => LogEvent;
 
     /// <summary>
     ///     Gets the timestamp when the log event occurred
     /// </summary>
     [UsedImplicitly]
-    public DateTimeOffset Timestamp => logEvent.Timestamp;
+    public DateTimeOffset Timestamp => LogEvent.Timestamp;
 
     /// <summary>
     ///     Gets the level of the log event (e.g., Information, Warning, Error)
     /// </summary>
     [UsedImplicitly]
-    public LogEventLevel Level => logEvent.Level;
+    public LogEventLevel Level => LogEvent.Level;
 
     /// <summary>
     ///     Gets the rendered message of the log event
     /// </summary>
     [UsedImplicitly]
-    public string Message => logEvent.RenderMessage(CultureInfo.InvariantCulture);
+    public string Message => LogEvent.RenderMessage(CultureInfo.InvariantCulture);
 }
