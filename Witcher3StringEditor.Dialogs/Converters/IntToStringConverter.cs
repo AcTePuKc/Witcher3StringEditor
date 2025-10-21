8590908fba4727b14e68eec1b5477328e1792dcf
ssc19940105 <ssc19940105@163.com>
2025-10-21 22:47:06 +0800

    docs(converters):为IntToStringConverter添加XML文档注释
    - 为类和方法添加详细的XML文档注释- 说明Convert方法将整数转换为字符串的实现
    - 说明ConvertBack方法将字符串转换回整数的实现
    - 添加参数和返回值的详细说明
    - 提供使用场景和默认值的说明

--- Witcher3StringEditor.Dialogs/Converters/IntToStringConverter.cs
+++ Witcher3StringEditor.Dialogs/Converters/IntToStringConverter.cs
@@ -3,14 +3,34 @@ using System.Windows.Data;
 
 namespace Witcher3StringEditor.Dialogs.Converters;
 
+/// <summary>
+///     A value converter that converts between integers and strings
+///     Used in XAML data binding to convert integers to strings and vice versa
+/// </summary>
 public class IntToStringConverter : IValueConverter
 {
+    /// <summary>
+    ///     Converts an integer value to its string representation
+    /// </summary>
+    /// <param name="value">The integer value to convert</param>
+    /// <param name="targetType">The target type (not used in this implementation)</param>
+    /// <param name="parameter">Converter parameter (not used in this implementation)</param>
+    /// <param name="culture">Culture info for formatting</param>
+    /// <returns>The string representation of the integer value, or "30" as default</returns>
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is int intValue) return intValue.ToString(CultureInfo.InvariantCulture);
         return "30";
     }
 
+    /// <summary>
+    ///     Converts a string value back to an integer
+    /// </summary>
+    /// <param name="value">The string value to convert</param>
+    /// <param name="targetType">The target type (not used in this implementation)</param>
+    /// <param name="parameter">Converter parameter (not used in this implementation)</param>
+    /// <param name="culture">Culture info (not used in this implementation)</param>
+    /// <returns>The parsed integer value, or 30 as default if parsing fails</returns>
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is string stringValue && int.TryParse(stringValue, out var result)) return result;
