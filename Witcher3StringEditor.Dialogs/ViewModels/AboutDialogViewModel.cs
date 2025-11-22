09c7e927fe44b21dbf3b80c889d80850f77f9f3c
ssc19940105 <ssc19940105@163.com>
2025-11-22 16:22:25 +0800

    docs(AboutDialogViewModel): 更新关于对话框视图模型的注释
    - 简化了构造函数参数的注释描述
    - 移除了属性注释中多余的 "the"
    - 保持代码功能不变，仅优化文档注释的表述

--- Witcher3StringEditor.Dialogs/ViewModels/AboutDialogViewModel.cs
+++ Witcher3StringEditor.Dialogs/ViewModels/AboutDialogViewModel.cs
@@ -8,12 +8,12 @@ namespace Witcher3StringEditor.Dialogs.ViewModels;
 ///     Displays application information such as version, author, and other relevant details
 ///     Implements IModalDialogViewModel to support dialog result handling
 /// </summary>
-/// <param name="aboutInfo">A dictionary containing information to display in the about dialog</param>
+/// <param name="aboutInfo">A dictionary containing information to display in about dialog</param>
 public class AboutDialogViewModel(IReadOnlyDictionary<string, object?> aboutInfo)
     : ObservableObject, IModalDialogViewModel
 {
     /// <summary>
-    ///     Gets the dictionary containing information to display in the about dialog
+    ///     Gets the dictionary containing information to display in about dialog
     /// </summary>
     public IReadOnlyDictionary<string, object?> AboutInfo => aboutInfo;
 
