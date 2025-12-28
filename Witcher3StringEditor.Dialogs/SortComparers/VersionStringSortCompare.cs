d467d5595d9145888d3fe1e82c609f3999350c66
ssc19940105 <ssc19940105@163.com>
2025-12-28 16:04:37 +0800

    Revert "feat(sort): 添加版本字符串排序比较器功能"
    This reverts commit 768fdb668dfda94617c23b50b6abe6fed6586f6e.

--- Witcher3StringEditor.Dialogs/SortComparers/VersionStringSortCompare.cs
+++ Witcher3StringEditor.Dialogs/SortComparers/VersionStringSortCompare.cs
@@ -4,43 +4,27 @@ using Syncfusion.Data;
 
 namespace Witcher3StringEditor.Dialogs.SortComparers;
 
-/// <summary>
-///     A custom comparer for sorting RuntimeLibrary objects based on their version strings
-/// </summary>
-public class VersionStringSortCompare : IComparer<RuntimeLibrary>, ISortDirection
+public class VersionStringSortCompare:IComparer<RuntimeLibrary>,ISortDirection
 {
-    /// <summary>
-    ///     Compares two RuntimeLibrary objects based on their version strings
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <returns></returns>
     public int Compare(RuntimeLibrary? x, RuntimeLibrary? y)
     {
-        // Extract version strings from RuntimeLibrary objects
         var versionStringX = x!.Version;
         var versionStringY = y!.Version;
-
+        
         try
         {
-            // Parse version strings into Version objects
             var versionX = new Version(versionStringX);
             var versionY = new Version(versionStringY);
 
-            var comparisonResult = versionX.CompareTo(versionY); // Compare versions
-            return SortDirection == ListSortDirection.Descending
-                ? -comparisonResult
-                : comparisonResult; // Return comparison result
+            var comparisonResult = versionX.CompareTo(versionY);
+
+            return SortDirection == ListSortDirection.Descending ? -comparisonResult : comparisonResult;
         }
         catch (Exception)
         {
-            return string.Compare(versionStringX, versionStringY,
-                StringComparison.Ordinal); // Compare version strings if parsing fails
+            return string.Compare(versionStringX, versionStringY, StringComparison.Ordinal);
         }
     }
 
-    /// <summary>
-    ///     Gets or sets the sort direction for the comparison
-    /// </summary>
     public ListSortDirection SortDirection { get; set; }
 }
