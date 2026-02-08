026a43f41dc7a9831397ab54a3991a91c4962f07
Shteryan Nikolaev <actepukc@gmail.com>
2026-02-08 15:39:48 +0000

    Add translation mode selector to dialog

---
+++ Witcher3StringEditor.Dialogs/ViewModels/TranslationMode.cs
@@ -0,0 +1,10 @@
+namespace Witcher3StringEditor.Dialogs.ViewModels;
+
+/// <summary>
+///     Controls whether translation uses legacy routing or provider routing.
+/// </summary>
+public enum TranslationMode
+{
+    Legacy,
+    Provider
+}
